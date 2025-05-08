1c17a9ed07feeb842262b4f1d4fc7363506a0be7
Simone-Domenici <domenici.simone00@gmail.com>
2025-05-08 16:06:13 +0200

    Add pizza detail view and update index page with details link

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,4 +27,12 @@ public class PizzaController {
         return "pizzas/index";
     }
     
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("pizza", repository.findById(id).get());
+
+        return "pizzas/show";
+
+    }
 }

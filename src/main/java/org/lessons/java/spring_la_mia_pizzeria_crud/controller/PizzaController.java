d41754dcf7cb81dcf06abdf16e908b27a336f547
Simone-Domenici <domenici.simone00@gmail.com>
2025-05-21 16:17:00 +0200

    Implement user authentication and authorization with roles; add security configuration, user and role models, and update views for role-based access

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -9,6 +9,7 @@ import org.lessons.java.spring_la_mia_pizzeria_crud.repository.OfferRepository;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.lessons.java.spring_la_mia_pizzeria_crud.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -38,11 +39,12 @@ public class PizzaController {
     private IngredientRepository ingredientRepository;
 
     @GetMapping
-    public String index(Model model) {
+    public String index(Authentication authentication, Model model) {
 
         List<Pizza> pizzas = pizzaService.findAll();
         
         model.addAttribute("pizzas", pizzas);
+        model.addAttribute("username", authentication.getName());
         return "pizzas/index";
     }
     
